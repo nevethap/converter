50a6c787e5358abbb5feec3905877e488d80b17f
nevethap <nevethap@INnevethap.local>
2015-12-23 14:11:19 +0530

    added mocha test to grunt to automate execution of test

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,9 +11,21 @@ module.exports = function(grunt) {
             target : {
                 src : ['src/app/celsiusToFahrenheit.js']
             }
-    }
+    },
+    mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec',
+                    //captureFile: 'results.txt', // Optionally capture the reporter output to a file
+                    //quiet: false, // Optionally suppress output to standard out (defaults to false)
+                    clearRequireCache: false // Optionally clear the require cache before running tests (defaults to false)
+                },
+                src: ['tests/**/*.js']
+            }
+      }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-execute');
-  grunt.registerTask('default', ['jshint', 'execute']);
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.registerTask('default', ['jshint', 'mochaTest', 'execute']);
 };
